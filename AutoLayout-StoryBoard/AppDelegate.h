1472983a85a62afad652c61fd28dd4afefe8a5b7
Kanjiviva <stevesun0123@email.com>
2015-09-01 16:31:16 -0700

    initial commit

---
+++ AutoLayout-StoryBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayout-StoryBoard
+//
+//  Created by Steve on 2015-09-01.
+//  Copyright (c) 2015 Steve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
